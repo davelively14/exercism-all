e8b3f67cf9803ffd08aec935f07d28aa75b53510
Dave Lively <davelively14@gmail.com>
2016-12-14 12:37:16 -0500

    Completed anagram

--- anagram/anagram.exs
+++ anagram/anagram.exs
@@ -3,7 +3,31 @@ defmodule Anagram do
   Returns all candidates that are anagrams of, but not equal to, 'base'.
   """
   @spec match(String.t, [String.t]) :: [String.t]
-  def match(base, candidates) do
+  def match(base, candidates), do: match(base, candidates, [])
+  def match(_base, [], result), do: result
 
+  # Couldn't use guard clauses because we can't invoke remote functions within
+  # them. Used cond instead.
+  def match(base, [head | tail], result) do
+    cond do
+      String.length(head) != String.length(base) ->
+        match(base, tail, result)
+      String.downcase(base) == String.downcase(head) ->
+        match(base, tail, result)
+      true ->
+        word_a = base |> clean_string
+        word_b = head |> clean_string
+
+        if word_a == word_b do
+          match(base, tail, result ++ [head])
+        else
+          match(base, tail, result)
+        end
+
+    end
+  end
+
+  defp clean_string(string) do
+    string |> String.downcase |> String.graphemes |> Enum.sort |> to_string
   end
 end
