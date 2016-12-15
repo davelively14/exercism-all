1d2431e69dfb174cbd1b6dd9f491e1b7a2d852ca
Dave Lively <davelively14@gmail.com>
2016-12-15 18:11:21 -0500

    Completed isogram

--- isogram/isogram.exs
+++ isogram/isogram.exs
@@ -3,8 +3,21 @@ defmodule Isogram do
   Determines if a word or sentence is an isogram
   """
   @spec isogram?(String.t) :: boolean
-  def isogram?(sentence) do
-
+  def isogram?(sentence), do: isogram?(sentence |> clean_list, [])
+  def isogram?([], _tracker), do: true
+  def isogram?([head | tail], tracker) do
+    if in_tracker(head, tracker) do
+      false
+    else
+      isogram?(tail, [head | tracker])
+    end
   end
 
+  defp clean_list(string) do
+    string |> String.downcase |> String.graphemes |> Enum.filter(&(&1 =~ ~r/[\p{L}]/))
+  end
+
+  defp in_tracker(item, []), do: false
+  defp in_tracker(item, [head | tail]) when head == item, do: true
+  defp in_tracker(item, [head | tail]), do: in_tracker(item, tail)
 end
