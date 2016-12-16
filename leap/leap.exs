4fa3cdb0bb67bd97cb98cffc019177eb6841ac24
Dave Lively <davelively14@gmail.com>
2016-12-16 17:55:06 -0500

    Fixed unused year

--- leap/leap.exs
+++ leap/leap.exs
@@ -12,5 +12,5 @@ defmodule Year do
   def leap_year?(year) when rem(year, 400) == 0, do: true
   def leap_year?(year) when rem(year, 100) == 0, do: false
   def leap_year?(year) when rem(year, 4) != 0, do: false
-  def leap_year?(year), do: true
+  def leap_year?(_year), do: true
 end
