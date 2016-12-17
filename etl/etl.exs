d293ee135d05f090a4cbad22d080b7c74b984e1d
Dave Lively <davelively14@gmail.com>
2016-12-17 09:15:54 -0500

    Completed etl

--- etl/etl.exs
+++ etl/etl.exs
@@ -9,6 +9,16 @@ defmodule ETL do
   """
   @spec transform(map) :: map
   def transform(input) do
+    keys = Map.keys(input)
+    transform(keys, input, %{})
+  end
+  def transform([], _input, new_map), do: new_map
+  def transform([head | tail], input, new_map) do
+    transform(tail, input, insert_new_values(Map.get(input, head), head, new_map))
+  end
 
+  defp insert_new_values([], _value, new_map), do: new_map
+  defp insert_new_values([head | tail], value, new_map) do
+    insert_new_values(tail, value, Map.put(new_map, String.downcase(head), value))
   end
 end
