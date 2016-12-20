313319dce9d3c81e3b6596968c69e25156b189e8
Dave Lively <davelively14@gmail.com>
2016-12-20 10:08:03 -0500

    Added supervision...might be too complicated

--- parallel-letter-frequency/frequency.exs
+++ parallel-letter-frequency/frequency.exs
@@ -8,6 +8,40 @@ defmodule Frequency do
   """
   @spec frequency([String.t], pos_integer) :: map
   def frequency(texts, workers) do
+    Supervisor.start_child(Frequency.Supervisor, [])
+  end
+end
+
+defmodule Frequency.Worker do
+  def start_link(texts) do
+    GenServer.start_link(__MODULE__, texts)
+  end
+end
+
+defmodule Frequency.Supervisor do
+  use Supervisor
+
+  def start_link() do
+    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def init(_) do
+    opts = [strategy: :simple_one_for_one]
+    children = [worker(Frequency.worker, [])]
+
+    supervise([], opts)
+  end
+end
+
+defmodule Frequency.Server do
+  use GenServer
+  import Supervisor.Spec
+
+  #######
+  # API #
+  #######
+
+  def start_link() do
 
   end
 end
