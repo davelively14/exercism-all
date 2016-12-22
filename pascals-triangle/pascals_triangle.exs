cea4d9c490eac2adf68bbce194740c677fbd5033
Dave Lively <davelively14@gmail.com>
2016-12-22 14:43:51 -0500

    Completed

--- pascals-triangle/pascals_triangle.exs
+++ pascals-triangle/pascals_triangle.exs
@@ -4,6 +4,16 @@ defmodule PascalsTriangle do
   with the given height
   """
   @spec rows(integer) :: [[integer]]
-  def rows(num) do
+  def rows(num), do: rows(num, [[1]])
+  def rows(1, result), do: result |> Enum.reverse
+  def rows(num, result = [last_row | _tail]) do
+    row = get_next_row(last_row)
+    rows(num - 1, [row | result])
+  end
+
+  defp get_next_row(last_row), do: get_next_row(last_row, [1])
+  defp get_next_row([head | tail], result) when length(tail) == 0, do: [1 | result] |> Enum.reverse
+  defp get_next_row([head | tail], result) do
+    get_next_row(tail, [head + (List.first(tail)) | result])
   end
 end
