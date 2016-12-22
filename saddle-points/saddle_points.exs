bbd17c9b0aac81190d231db0bb695315a6bf7eb5
Dave Lively <davelively14@gmail.com>
2016-12-22 18:03:57 -0500

    Completed

--- saddle-points/saddle_points.exs
+++ saddle-points/saddle_points.exs
@@ -5,7 +5,9 @@ defmodule Matrix do
   """
   @spec rows(String.t()) :: [[integer]]
   def rows(str) do
-
+    str
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, " ") |> Enum.map(fn (x) -> String.to_integer(x) end))
   end
 
   @doc """
@@ -13,16 +15,33 @@ defmodule Matrix do
   to a list of columns
   """
   @spec columns(String.t()) :: [[integer]]
-  def columns(str) do
-
-  end
+  def columns(str), do: str |> rows |> List.zip |> Enum.map(&Tuple.to_list(&1))
 
   @doc """
   Calculates all the saddle points from a string
   representation of a matrix
   """
+
+  # {row, column} <- starts at 0
   @spec saddle_points(String.t()) :: [{integer, integer}]
   def saddle_points(str) do
+    all_rows = str |> rows
+    all_cols = str |> columns
+    check_by_rows(all_rows, all_cols)
+  end
 
+  def check_by_rows(all_rows, all_cols), do: check_by_rows(all_rows, all_cols, [], 0)
+  def check_by_rows([], _, results, _), do: results |> Enum.reverse
+  def check_by_rows([row | rows_tail], all_cols, results, row_num) do
+    check_by_rows(rows_tail, all_cols, check_row(row, Enum.max(row), row_num, all_cols, results, 0), row_num + 1)
+  end
+
+  def check_row([], _, _, _, results, _), do: results
+  def check_row([value | row_tail], max_in_row, row_num, cols, results, col_num) do
+    if value >= max_in_row && value <= Enum.min(Enum.at(cols, col_num)) do
+      check_row(row_tail, max_in_row, row_num, cols, [{row_num, col_num} | results], col_num + 1)
+    else
+      check_row(row_tail, max_in_row, row_num, cols, results, col_num + 1)
+    end
   end
 end
