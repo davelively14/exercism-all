6a896ec570c95c1f0c178f6a9477965a114ea78b
Dave Lively <davelively14@gmail.com>
2016-12-21 23:12:18 -0500

    Completed

--- queen-attack/queen_attack.exs
+++ queen-attack/queen_attack.exs
@@ -5,10 +5,14 @@ defmodule Queens do
   @doc """
   Creates a new set of Queens
   """
-  @spec new() :: Queens.t()
+  # @spec new() :: Queens.t()
   @spec new({integer, integer}, {integer, integer}) :: Queens.t()
-  def new(white, black) do
-
+  def new(white \\ {0, 3}, black \\ {7, 3}) do
+    if white == black do
+      raise(ArgumentError)
+    else
+      %{white: white, black: black}
+    end
   end
 
   @doc """
@@ -17,7 +21,20 @@ defmodule Queens do
   """
   @spec to_string(Queens.t()) :: String.t()
   def to_string(queens) do
+    board = String.strip """
+    _ _ _ _ _ _ _ _
+    _ _ _ _ _ _ _ _
+    _ _ _ _ _ _ _ _
+    _ _ _ _ _ _ _ _
+    _ _ _ _ _ _ _ _
+    _ _ _ _ _ _ _ _
+    _ _ _ _ _ _ _ _
+    _ _ _ _ _ _ _ _
+    """
 
+    board
+    |> place_queen(queens.white, "W")
+    |> place_queen(queens.black, "B")
   end
 
   @doc """
@@ -25,6 +42,16 @@ defmodule Queens do
   """
   @spec can_attack?(Queens.t()) :: boolean
   def can_attack?(queens) do
+    cond do
+      queens.white |> elem(0) == queens.black |> elem(0) -> true
+      queens.white |> elem(1) == queens.black |> elem(1) -> true
+      abs(elem(queens.white, 0) - elem(queens.black, 0)) == abs(elem(queens.white, 1) - elem(queens.black, 1)) -> true
+      true -> false
+    end
+  end
 
+  def place_queen(board, {x, y}, color) do
+    position = (x * 16) + (y * 2)
+    String.slice(board, 0, position) <> color <> String.slice(board, position + 1, String.length(board))
   end
 end
