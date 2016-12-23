fe9ff6f54add4a9dbf7f349fd96b1182a140f03c
Dave Lively <davelively14@gmail.com>
2016-12-23 14:23:02 -0500

    Completed

--- binary-search/binary_search.exs
+++ binary-search/binary_search.exs
@@ -18,7 +18,9 @@ defmodule BinarySearch do
   """
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
-  def search(numbers, key) do
-
+  def search(numbers, key), do: search(Tuple.to_list(numbers), key, 0)
+  def search([], _key, _counter), do: :not_found
+  def search([head | tail], key, counter) do
+    if head == key, do: {:ok, counter}, else: search(tail, key, counter + 1)
   end
 end
