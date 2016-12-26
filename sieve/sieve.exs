cf042494a1ef36541da449a3f0af0f58f1c71608
Dave Lively <davelively14@gmail.com>
2016-12-25 20:41:46 -0500

    Removed unnecessary function. Using Enum.to_list instead

--- sieve/sieve.exs
+++ sieve/sieve.exs
@@ -4,13 +4,9 @@ defmodule Sieve do
   Generates a list of primes up to a given limit.
   """
   @spec primes_to(non_neg_integer) :: [non_neg_integer]
-  def primes_to(limit), do: limit |> gen_list |> primes_to(limit, [])
+  def primes_to(limit), do: primes_to(Enum.to_list(2..limit), limit, [])
   def primes_to([], _limit, result), do: result |> Enum.reverse
   def primes_to([head | tail], limit, result), do: primes_to(remove_multiples(head, tail), limit, [head | result])
-
-  def gen_list(limit), do: gen_list(limit, limit, [])
-  def gen_list(_limit, counter, result) when counter < 2, do: result
-  def gen_list(limit, counter, result), do: gen_list(limit, counter - 1, [counter | result])
 
   def remove_multiples(base, list), do: remove_multiples(base, list, [])
   def remove_multiples(_base, [], result), do: result |> Enum.reverse
