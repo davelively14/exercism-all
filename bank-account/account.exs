1e7102bc739f320a95cbd56c84b7e289037b947e
Dave Lively <davelively14@gmail.com>
2016-12-28 17:44:18 -0430

    Temp

--- bank-account/account.exs
+++ bank-account/account.exs
@@ -1,4 +1,5 @@
 defmodule BankAccount do
+  use GenServer
   @moduledoc """
   A bank account that supports access from multiple processes.
   """
@@ -8,11 +9,19 @@ defmodule BankAccount do
   """
   @opaque account :: pid
 
+  #######
+  # API #
+  #######
+
   @doc """
   Open the bank. Makes the account available.
   """
   @spec open_bank() :: account
   def open_bank() do
+    unless is_online? do
+      GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+    end
+    GenServer.call(__MODULE__, :add_account)
   end
 
   @doc """
@@ -27,6 +36,7 @@ defmodule BankAccount do
   """
   @spec balance(account) :: integer
   def balance(account) do
+    GenServer.call(__MODULE__, {:balance, account})
   end
 
   @doc """
@@ -34,5 +44,28 @@ defmodule BankAccount do
   """
   @spec update(account, integer) :: any
   def update(account, amount) do
+  end
+
+  def is_online?, do: if GenServer.whereis(__MODULE__), do: true, else: false
+
+  #############
+  # Callbacks #
+  #############
+
+  def init do
+    new_state = %{accounts: [], counter: 0}
+    {:ok, new_state}
+  end
+
+  def handle_call(:add_account, _from, state) do
+    new_account = %{acct_num: state.counter + 1, balance: 0}
+    new_state =
+      Map.put(state, :accounts, [new_account | state.accounts])
+      |> Map.put(state, :counter, state.counter + 1)
+    {:ok, new_state.counter, new_state}
+  end
+
+  def handle_call({:balance, account}, _from, state) do
+    {:reply, state.balance, state}
   end
 end
