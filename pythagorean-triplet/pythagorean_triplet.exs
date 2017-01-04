70c00ca866ee229aef21216ecf059dfd18bdf805
Dave Lively <davelively14@gmail.com>
2017-01-04 10:31:22 -0400

    Partial, but first generate function works

--- pythagorean-triplet/pythagorean_triplet.exs
+++ pythagorean-triplet/pythagorean_triplet.exs
@@ -26,13 +26,19 @@ defmodule Triplet do
   Generates a list of pythagorean triplets from a given min (or 1 if no min) to a given max.
   """
   @spec generate(non_neg_integer, non_neg_integer) :: [list(non_neg_integer)]
-  def generate(min, max), do: generate(min, max, [min, min + 1, min * min + (min + 1) * (min + 1)])
-  def generate(min, max, [a, b, c_raw]) do
-    IO.inspect %{a: a, b: b, c_raw: c_raw}
+  def generate(min, max), do: inspect_this(min, max, [])
+  def inspect_this(min, max, result) when min == max, do: result |> Enum.filter(&(&1 != []))
+  def inspect_this(min, max, result) do
+    # IO.inspect %{min: min, max: max, result: result}
+    inspect_this(min + 1, max, result ++ [check_all(min, min + 1, max)])
+  end
+
+  def check_all(a, b, max) do
+    # IO.inspect %{a: a, b: b, c: :math.sqrt(a*a + b*b)}
     cond do
-      :math.sqrt(c_raw) > max -> [0, 0, 0]
-      rem(:math.sqrt(c_raw) * 10 |> round, 10) == 0 -> [a, b, :math.sqrt(c_raw) |> round]
-      true -> generate(min, max, [a + 1, b + 1, (a + 1) * (a + 1) + (b + 1) * (b + 1)])
+      :math.sqrt(a*a + b*b) > max -> []
+      rem(:math.sqrt(a*a + b*b) * 10 |> round, 10) == 0 -> [a, b, :math.sqrt(a*a + b*b) |> round]
+      true -> check_all(a, b + 1, max)
     end
   end
 
@@ -41,5 +47,17 @@ defmodule Triplet do
   """
   @spec generate(non_neg_integer, non_neg_integer, non_neg_integer) :: [list(non_neg_integer)]
   def generate(min, max, sum) do
+    triplets = generate(min, max)
+    check_trips_vs_sum(triplets, sum)
+  end
+
+  def check_trips_vs_sum(triplets, sum), do: check_trips_vs_sum(triplets, sum, [])
+  def check_trips_vs_sum([], _sum, result), do: result
+  def check_trips_vs_sum([head | tail], sum, result) do
+    IO.inspect %{head: head, sum: sum, result: result}
+    cond do
+      sum(head) == sum -> check_trips_vs_sum(tail, sum, result ++ head)
+      true -> check_trips_vs_sum(tail, sum, result)
+    end
   end
 end
--- pythagorean-triplet/pythagorean_triplet_test.exs
+++ pythagorean-triplet/pythagorean_triplet_test.exs
@@ -42,7 +42,7 @@ defmodule PythagoreanTripletTest do
     assert Enum.map(triplets, &Triplet.product/1) == [3840]
   end
 
-  @tag :pending
+
   test "triplets where sum is 180 and max factor is 100" do
     triplets = Triplet.generate(1, 100, 180)
     assert Enum.map(triplets, &Triplet.product/1) == [118080, 168480, 202500]
